8720f2592e8800d0f030bff3de58a4b8120a8b3a
natiteg <natitegg@gmail.com>
2022-09-08 18:35:16 -0700

    commit 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include<stdio.h>
 
-
-
 /**
-
- * main - Function that print a text.
-
- * Return:  0.
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
-int main(void)
-  
+int main(void) 
 {
-  
   puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+  return (0); 
 }
