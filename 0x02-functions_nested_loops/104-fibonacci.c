972e9ff26018383eb9e3f5396974c7ab6e0e0c28
natiteg <natitegg@gmail.com>
2022-09-13 15:53:18 -0700

    functions, nested loops.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,18 +7,40 @@
 */
 int main(void)
 {
-int i = 0;
-unsigned long int a = 0, b = 1, next = 0;
-while (i < 98)
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
 {
-next = a + b;
-a = b;
-b = next;
-printf("%lu", next);
-if (i < 97)
-printf(", ");
-i++;
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 = sum;
 }
-putchar('\n');
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+for (count = 93; count < 99; count++)
+{
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
+}
+
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
