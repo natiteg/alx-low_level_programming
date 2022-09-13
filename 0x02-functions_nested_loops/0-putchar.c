d30e0ea224cac4a15147b7251947bcc19436bf89
natiteg <natitegg@gmail.com>
2022-09-13 13:46:48 -0700

    functions, nested loops.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,18 @@
-#include"main.h"
-
+#include "main.h"
 /**
-* main - entry point
-*
-* Description: prints _putchar using putchar prototype
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-_putchar("_");
-_putchar("p");
-_putchar("u");
-_putchar("t");
-_putchar("c");
-_putchar("h");
-_putchar("a");
-_putchar("r");
-_putchar("\n");
+char *sh = "_putchar";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
 return (0);
 }
+
