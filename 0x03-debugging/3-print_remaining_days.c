61d75c552943b6653ba90a4fbf5d69c471d51b13
natiteg <natitegg@gmail.com>
2022-09-15 12:53:50 -0700

    Debug push

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,7 +29,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
-print("Day of the year: %d\n", day);
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
