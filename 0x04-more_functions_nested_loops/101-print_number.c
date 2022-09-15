25f90e6c300b6df1f5898775e2cc0e12d8a7f8f5
natiteg <natitegg@gmail.com>
2022-09-15 15:32:17 -0700

    Debug push

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,26 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* print_number - prints numbers
-* @n: number to be printed
-* Return:void
+* print_number - print an integer (with no trailing newline)
+* @n: integer to print
+*
+* Return: void
 */
 void print_number(int n)
 {
-int d = 1, i = 0, ii = 0;
-if (n < 0)
+unsigned int m;
+if (n >= 0)
+{
+if (n >= 10)
+print_number(n / 10);
+_putchar('0' + n % 10);
+}
+else
 {
 _putchar('-');
-n = -n;
+m = -1 * n;
+if (m >= 10)
+print_number(m / 10);
+_putchar('0' + m % 10);
 }
-while (n / d != 0)
-{
-d *= 10;
-i++;
-}
-d = d / 10;
-while (ii < i)
-{
-_putchar('0' + n / d);
-n = n - (n / d) *d;
-d = d / 10;
-ii++;
-}
-if (i == 0)
-_putchar('0' + n);
 }
