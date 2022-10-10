905e7f9521b6975350d348a4e9b7145c0e294156
natiteg <natitegg@gmail.com>
2022-10-10 15:34:15 -0700

    avicii

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,9 +10,9 @@
 
 typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
