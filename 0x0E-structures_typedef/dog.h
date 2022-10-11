8c9c4595843cca28e9c63e0e995e300403902684
natiteg <natitegg@gmail.com>
2022-10-10 17:16:31 -0700

    avicii

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
 * struct dog - structure
@@ -8,14 +8,16 @@
 * @owner: owner
 */
 
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+
+#endif /*STRTYP*/
