58add9a64fa0470a9816ce515068ee029c7e2c4d
natiteg <natitegg@gmail.com>
2023-02-21 11:24:29 -0800

    Added testfiles

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - using write outputs a string to stderr
+ * main - using write prints a string to stderr
  * Return: (1) successful exit code for project spec
  */
 int main(void)
